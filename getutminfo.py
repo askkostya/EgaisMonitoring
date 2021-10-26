03624d9cbaf45f70a4e7c545a1631512078e126f
askkostya <askkostya@gmail.com>
2021-10-26 14:33:55 +1200

    Частичный переход API УТМ swagger, не нужно больше парсить текст

--- getutminfo.py
+++ getutminfo.py
@@ -4,22 +4,37 @@
 import datetime
 import requests
 import sys
+import time
 
 
 def deltadate(strData):
     now = datetime.datetime.now()
-    DateofEND = datetime.datetime.strptime(strData, "%Y-%m-%d %H:%M:%S")
+    DateofEND = datetime.datetime.strptime(strData[:-6], "%Y-%m-%d %H:%M:%S")
     deltaindays = (DateofEND - now).days
     return deltaindays
 
-def getUTMUrl(getURL):
-    try:
-        user_agent = {'User-Agent': 'Mozilla/5.0'}
-        r = requests.get(getURL, headers=user_agent)
-        return (r.text)
-    except requests.exceptions.RequestException:
-    # Timeout or ConnectionError
-        sys.exit()
+
+def getUTMURL(getURL, contType):
+       try:
+         user_agent = {'User-Agent': 'Mozilla/5.0'}
+         r = requests.get(getURL, headers=user_agent)
+         if contType == 1:
+             return r.json()
+         elif contType == 2:
+             return r.text
+       except requests.exceptions.RequestException:
+     # Timeout or ConnectionError
+             return -1
+
+
+def getConnection(getURL, contType):
+    i = 1
+    while i < 5:
+        utm_data = getUTMURL(getURL, contType)
+        if utm_data != -1:
+            return utm_data
+        time.sleep(5)
+        i = i+1
 
 try:
     getURL = sys.argv[2]
@@ -27,35 +42,28 @@ except BaseException:
     getURL = "http://localhost:8080"
 
 if sys.argv[1] == "version":
-    httptext = getUTMUrl(getURL+'/info/version')
+    httptext = getConnection(getURL+'/info/version', 2)
     print(httptext)
 
 if sys.argv[1] == "rsavalid":
-    httptext = getUTMUrl(getURL)
-    ipnumstr = httptext.rfind("Проблемы с RSA:")
-    if ipnumstr == -1:
-        print("Valid")
-    else:
-        print("Invalid")
+    httptext = getConnection(getURL + '/api/info/list', 1)
+    rsaValid = httptext['rsa']['isValid']
+    print(rsaValid)
 
 if sys.argv[1] == "rsadate":
-    httptext = getUTMUrl(getURL+'/home')
-    ipnumstr = httptext.rfind("Сертификат RSA")
-    strRSA = (httptext[ipnumstr+77:ipnumstr + 140])
-    ipnumstr = strRSA.rfind("по")
-    RSAdatestr = (strRSA[ipnumstr + 3:ipnumstr + 22])
-    print(deltadate(RSAdatestr))
+    httptext = getConnection(getURL + '/api/info/list', 1)
+    expireDateStr = httptext['rsa']['expireDate']
+    expireDate = deltadate(expireDateStr)
+    print(expireDate)
 
 if sys.argv[1] == "gostdate":
-    httptext = getUTMUrl(getURL+'/home')
-    ipnumstr = httptext.rfind("Сертификат ГОСТ")
-    strGOST = (httptext[ipnumstr:ipnumstr + 140])
-    ipnumstr = strGOST.rfind("по")
-    GOSTdatestr = (strGOST[ipnumstr + 3:ipnumstr + 22])
-    print(deltadate(GOSTdatestr))
+    httptext = getConnection(getURL+'/api/info/list', 1)
+    expireDateStr = httptext['gost']['expireDate']
+    expireDate = deltadate(expireDateStr)
+    print(expireDate)
 
 if sys.argv[1] == "docsbuffer":
-    httptext = getUTMUrl(getURL+'/home')
+    httptext = getConnection(getURL+'/home', 2)
     ipnumstr = httptext.rfind("Отсутствуют неотправленные чеки")
     if ipnumstr == -1:
         ipnumstr = httptext.rfind("Чеки не отправлялись с")
